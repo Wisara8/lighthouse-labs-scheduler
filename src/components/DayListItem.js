d75b8f0edefff95d2a4e4d9792a5fdeb9bf57612
wisara88 <srandall845@gmail.com>
2019-12-05 23:52:07 -0800

    Button SCSS reference

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,3 +10,20 @@ export default function DayListItem(props) {
     </li>
   );
 }
+
+export default function Button(props) {
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+ 
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
