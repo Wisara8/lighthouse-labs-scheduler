c4cf108eb04bb1292728d51365def5567e064514
wisara88 <srandall845@gmail.com>
2019-12-08 14:06:47 -0800

    spots remaining conditionally rendering

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,12 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots 
   });
 
+  const formatSpots = (spots) => {
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return "no spots remaining";
+    }
+  }
+
   return (
     <li 
       className={dayClass}
       onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {/* <h3 className="text--light">{props.spots} spots remaining</h3> */}
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
