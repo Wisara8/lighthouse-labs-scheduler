f655b1777f52d2fde057fa4b3a35e2c140b8361e
wisara88 <srandall845@gmail.com>
2019-12-08 13:14:45 -0800

    SCSS rendering bug partially fixed

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,9 +3,9 @@ import "components/DayListItem.scss";
 var classnames = require('classnames');
 
 export default function DayListItem(props) {
-  const dayClass = classnames("dayList", {
-    "__item--selected": props.selected,
-    "__item--full": !props.spots 
+  const dayClass = classnames("day-list__item", {
+    "--selected": props.selected,
+    "--full": !props.spots 
   });
 
   return (
@@ -13,7 +13,7 @@ export default function DayListItem(props) {
       className={dayClass}
       onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
