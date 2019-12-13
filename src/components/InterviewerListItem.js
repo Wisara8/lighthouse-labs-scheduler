f42d53fada4e30c18cba8467008aac696584ce9a
wisara88 <srandall845@gmail.com>
2019-12-13 15:26:12 -0800

    testing InterviewerListItem.SCSS classnames

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,14 +2,13 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 var classnames = require('classnames');
 
-export default function DayListItem(props) {
-  const dayClass = classnames("day-__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": !props.spots 
+export default function InterviewerListItem(props) {
+  const interviewersClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
   });
 
   return (
-    <li className="interviewers__item">
+    <li className={interviewersClass}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
