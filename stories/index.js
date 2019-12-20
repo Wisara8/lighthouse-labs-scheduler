be324e908cdca279ca935fb48d45bb361568349c
wisara88 <srandall845@gmail.com>
2019-12-20 00:12:31 -0800

    Appointment components all roughed in

--- stories/index.js
+++ stories/index.js
@@ -19,6 +19,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -157,5 +159,12 @@ storiesOf("Button", module)
         message="Delete the appointment?"
         onConfirm={action("onConfirm")}
         onCancel={action("onCancel")}
+      />
+    ))
+    .add("Status", () => <Status message="Deleting"/>)
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment"
+        onClose={action("onClose")}
       />
     ))
