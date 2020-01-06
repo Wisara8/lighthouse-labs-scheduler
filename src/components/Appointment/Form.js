715a3d4ae28525a6eb64e59e1cc45709d6910cff
wisara88 <srandall845@gmail.com>
2020-01-05 23:29:57 -0800

    .

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,12 +21,14 @@ export default function Form(props) {
           />
         </form>
 
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
+
           <Button danger onClick={props.onCancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
+          
         </section>
       </section>
     </main>
