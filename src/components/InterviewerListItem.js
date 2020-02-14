bc086ff383f4faab8ea92d465456b15fa26ac8c1
wisara88 <srandall845@gmail.com>
2020-02-13 19:36:37 -0800

    .

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -18,3 +18,4 @@ export default function InterviewerListItem(props) {
     </li>
   );
 }
+
--- stories/index.js
+++ stories/index.js
@@ -63,7 +63,7 @@ const days = [
   },
   {
     id: 3,
-    name: "Wednesday",
+    name: "Friday",
     spots: 0,
   },
 ];
